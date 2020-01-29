a5e10c877642dae164b36795740d1972bba28f6a
Brian Mesick <bmesick@edx.org>
2020-01-29 16:30:17 -0500

    flake8 fixes

--- simple_history/models.py
+++ simple_history/models.py
@@ -574,7 +574,9 @@ class HistoricalRecords(object):
                 insert_row = {'history': history_instance}
 
                 for through_model_field in through_model._meta.fields:
-                    insert_row[through_model_field.name] = getattr(row,  through_model_field.name)
+                    insert_row[through_model_field.name] = getattr(
+                        row, through_model_field.name
+                    )
 
                 m2m_history_model.objects.create(**insert_row)
 
